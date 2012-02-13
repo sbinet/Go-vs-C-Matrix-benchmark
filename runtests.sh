4638c5d97625fea7987f3731d62277748397a002
Sebastien Binet <binet@cern.ch>
2012-02-13 14:39:09 +0100

    remove bound checking for go/sequential progs

--- runtests.sh
+++ runtests.sh
@@ -2,27 +2,29 @@
 
 function run_tests()
 {
+    progargs=(800 900 3000)
+
     echo ""
     echo ":::::::::::::::::::::::::::::"
     echo ":: c sequential-matrix..."
-    time ./C/sequential/sequential_matrix 800 900 3000
+    time ./C/sequential/sequential_matrix "${progargs[@]}"
     echo ""
     echo ":: gc sequential-matrix..."
-    time ./go/sequential/sequential_matrix 800 900 3000
+    time ./go/sequential/sequential_matrix "${progargs[@]}"
     echo ""
     echo ":: gccgo sequential-matrix..."
-    time ./go/sequential/sequential_matrix-gccgo 800 900 3000
+    time ./go/sequential/sequential_matrix-gccgo "${progargs[@]}"
 
     echo ""
     echo ":::::::::::::::::::::::::::::"
     echo ":: c sequential-vector..."
-    time ./C/sequential/sequential_vector 800 900 3000
+    time ./C/sequential/sequential_vector "${progargs[@]}"
     echo ""
     echo ":: gc sequential-vector..."
-    time ./go/sequential/sequential_vector 800 900 3000
+    time ./go/sequential/sequential_vector "${progargs[@]}"
     echo ""
     echo ":: gccgo sequential-vector..."
-    time ./go/sequential/sequential_vector-gccgo 800 900 3000
+    time ./go/sequential/sequential_vector-gccgo "${progargs[@]}"
 }
 
 function make_c_targets()
